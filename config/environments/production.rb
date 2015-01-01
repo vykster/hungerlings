b54318abefac24b493547c30a38a2089281052b5
Vyki <vyki@nationbuilder.com>
2014-12-31 21:13:41 -0700

    more heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   config.assets.precompile += ['static_pages.css']
   config.assets.initialize_on_precompile=false
